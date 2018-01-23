cfd31c666d21699bb88509f70c8b28dfc83f5a79
anand <ananddeb23@gmail.com>
2018-01-23 12:37:22 +0530

    check for excess values in case of strikes at the end

--- index.test.js
+++ index.test.js
@@ -3,12 +3,15 @@ let index = require('./index');
 describe('Output type being tested', () =>{
 	let ar1 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
 	let ar2 = [10,10,10,10,10,10,10,10,10,10];
-
+	let ar3 = [3,6,4,6,10,10,10,5,3,6,4,7,0,1,2,10,9,10,5];
 	it('Return type must be a number when given correct sequence', () => {
 		expect(typeof(index.score(ar1))).toBe('number');
 	});
-	it('Return type must be a number when given in-correct sequence', () => {
+	it('Return type must be a number when given in-correct sequence with missing numbers', () => {
 		expect(typeof(index.score(ar2))).toBe('number');
+	});
+	it('Return type must be a number when given in-correct sequence with excess numbers', () => {
+		expect(typeof(index.score(ar3))).toBe('number');
 	});
 
 });
@@ -37,6 +40,8 @@ describe('Input must be validated', () =>{
 	let ar3 = [10,10,10,10,10,10,10,10,10,10];
 	let ar4 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3];
 	let ar5 = [9,1,6,4,7,3,5,5,3,7,2,8,1,9,2,8,9,1,7,3];
+	let ar6 = [10,10,10,10,10,10,10,10,10,10,10,9,4];
+	let ar7 = [9,1,6,4,7,3,5,5,3,7,2,8,1,9,2,8,9,1,7,3,8,6];
 	it('In case of missing values  when there is a strike at the end return -1', () => {
 		expect((index.score(ar3))).toEqual(-1);
 	});
@@ -50,5 +55,9 @@ describe('Input must be validated', () =>{
 	it('In case of excess values return -2', () => {
 		expect((index.score(ar2))).toEqual(-2);
 	});
+	it('In case of excess values with all strikes return -2', () => {
+		expect((index.score(ar6))).toEqual(-2);
+	});
+	
 
 });
