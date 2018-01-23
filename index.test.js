cecd392125605e49734840daad048309f9e6f7a4
anand <ananddeb23@gmail.com>
2018-01-23 11:52:25 +0530

    test case for checking correct score when only  spares are present

--- index.test.js
+++ index.test.js
@@ -11,14 +11,18 @@ describe('Differnt frame combinations as inputs must return correct score', () =
 	let ar1 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
 	let ar2 = [3,6,4,6,10,10,10,5,3,6,4,7,0,1,2,10,9,10];
 	let ar3 = [10,10,10,10,10,10,10,10,10,10,10,10];
+	let ar4 = [9,1,6,4,7,3,5,5,3,7,2,8,1,9,2,8,9,1,7,3,6];
 	it('Frames without any strikes or spare must have must return correct score', () => {
 		expect(index.score(ar1)).toEqual(90);
 	});
 	it('Frames with any strikes and spare must have must return correct score', () => {
 		expect(index.score(ar2)).toEqual(166);
 	});
-	it('Frames with any strikes and spare must have must return correct score', () => {
+	it('Frames with all strikes  must have must return correct score', () => {
 		expect(index.score(ar3)).toEqual(300);
+	});
+	it('Frames with all spares  must have must return correct score', () => {
+		expect(index.score(ar4)).toEqual(148);
 	});
 
 });
