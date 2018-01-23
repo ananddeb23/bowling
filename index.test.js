7543d0a039e85e58dfc9cded7bae1754e7c89f0c
anand <ananddeb23@gmail.com>
2018-01-23 11:50:27 +0530

    test case for checking correct score when only  strikes are present

--- index.test.js
+++ index.test.js
@@ -10,11 +10,15 @@ describe('Output type being tested', () =>{
 describe('Differnt frame combinations as inputs must return correct score', () =>{
 	let ar1 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
 	let ar2 = [3,6,4,6,10,10,10,5,3,6,4,7,0,1,2,10,9,10];
+	let ar3 = [10,10,10,10,10,10,10,10,10,10,10,10];
 	it('Frames without any strikes or spare must have must return correct score', () => {
 		expect(index.score(ar1)).toEqual(90);
 	});
 	it('Frames with any strikes and spare must have must return correct score', () => {
 		expect(index.score(ar2)).toEqual(166);
+	});
+	it('Frames with any strikes and spare must have must return correct score', () => {
+		expect(index.score(ar3)).toEqual(300);
 	});
 
 });
